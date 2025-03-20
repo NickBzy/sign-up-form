d2ef5b7d4f87228ed372afefe9497c186848c4d1
Nicolas B <97320296+NickBzy@users.noreply.github.com>
2025-03-20 19:00:48 -0400

    Add password matching check

--- scripts/script.js
+++ scripts/script.js
@@ -4,3 +4,20 @@ const emailInput = document.getElementById("phone");
 emailInput.addEventListener("input", function (event) {
     this.value = this.value.replace(/\D/g, "");
 });
+
+const password = document.getElementById("password");
+const confirmPassword = document.getElementById("confirm-password");
+const errorMessage = document.getElementById("error-message");
+
+function checkPasswordMatch() {
+    if (password.value !== confirmPassword.value) {
+        confirmPassword.setCustomValidity("Passwords do not match");
+        errorMessage.style.display = "block";
+    } else {
+        confirmPassword.setCustomValidity("");
+        errorMessage.style.display = "none";
+    }
+}
+
+password.addEventListener("input", checkPasswordMatch);
+confirmPassword.addEventListener("input", checkPasswordMatch);
