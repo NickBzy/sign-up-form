c7767a55019e8f2ca95a43531a825e83651b8f72
Nicolas B <97320296+NickBzy@users.noreply.github.com>
2025-03-20 18:48:56 -0400

    Add styling for right side container

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,6 @@
 console.log("Hello World!")
+
+const emailInput = document.getElementById("phone");
+emailInput.addEventListener("input", function (event) {
+    this.value = this.value.replace(/\D/g, "");
+});
